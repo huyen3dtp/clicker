30e9d9986aa44c94d89ba31381d746cb46d40c69
hoangsonb <emchimto183@gmail.com>
2021-07-08 21:38:37 +0700

    jj đó

---
+++ hw3.js
@@ -0,0 +1,22 @@
+const dog = {
+    name : "Bark",
+    color : "Brown",
+    weight : 5
+}
+const rabbit = {
+    name : "Ribbit",
+    color : "white",
+    weight : 1
+}
+const cat = {
+    name : "Meow",
+    color : "Yellow",
+    weight : 4
+}
+dog.weight += 1
+console.log(dog.weight)
+console.log(rabbit.name)
+console.log(cat.color)
+
+dog.species = "Poodle"
+console.log
---
+++ hw333.js
@@ -0,0 +1,11 @@
+let N = window.prompt("Nhap chieu dai N")
+let ans = ""
+
+for (let i = 0 ; i<N;i++){
+    for(let j = 0;j<N;j++){
+        ans += "*"
+    }
+    ans += "\n"
+}
+console.log(ans)
+
